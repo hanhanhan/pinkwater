101bec40a0ff7216bf0a9f44fb60180d0fb96edb
Hannah Lazarus <han.lazarus@gmail.com>
2016-04-21 03:08:26 -0700

    Program fully outlined

--- pinkwater_mp3_scrape.py
+++ pinkwater_mp3_scrape.py
@@ -26,12 +26,12 @@ def get_book_list():
     strainer = SoupStrainer('option')
     indexsoup = BeautifulSoup(r.text, 'lxml', parse_only=strainer)
 
-    audiobook_index = [ book['value'] for book in indexsoup.find_all('option')]
+    book_options = [ book['value'] for book in indexsoup.find_all('option')]
 
     with open(booklist, 'w') as f:
-        json.dump(audiobook_index, f)
+        json.dump(book_options, f)
 
-    return audiobook_index
+    return book_options
 
 def get_book_page(book_option):
     ''' Does not return book specific page with mp3 file index
@@ -99,95 +99,21 @@ def make_book_directory(book):
     if not os.path.exists(audiobook_subdirectory):
         os.mkdir(audiobook_subdirectory)
 
-    return(audiobook_subdirectory)
+    return audiobook_subdirectory
 
 def main():
+    ''' Get audiobook files from Daniel Pinkwater's site and save in 
     '''
-    Get options for books
-    send book selection in header
-    Get URLs of each book chapter
-    Cache in file
-    make folder for book in directory if doesn't exist
-    check if chapter mp3 file exists
-    get if doesn't exist  
-    '''
+    #Get list of book options from drop down menu on main page
+    book_options = get_book_list()
 
-    book_index = get_book_index()
-
-    for book in book_index:
-        pass
-        #Get urls of mp3 files from page.
-        #cache mp3 urls in subdirectory
-        #get mp3 files for each book and stash in book directory
-
-
-
-    '''
-        book_elem = browser.find_elements_by_tag_name('option')
-
-
-
-    #I tried to write this more elegantly as
-    #for book in book_elem:
-    #but that didn't work at all -- because it's not the right type of object?
-    print("Type of book_elem is ",type(book_elem))
-    print("Type of range(len(book_elem)) is ", type(range(len(book_elem))))
-
-    for book in range(len(book_elem)):
-    #for book in range(min(3,len(book_elem))):
-        book_elem[book].click()
-        book_elem[book].submit()
-
-        r = requests.get(browser.current_url)
-        booksoup = BeautifulSoup(r.text, 'html.parser')
-
-
-        #Get links for each chapter
-        #It would be better to search by .mp3 per page
-        #this disappears unless I recreate it. why?
-        mp3_links = browser.find_elements_by_partial_link_text('Chapter')
-        if len(mp3_links) < 1:
-            mp3_links = browser.find_elements_by_partial_link_text('ntire book')
-        if len(mp3_links) < 1:
-            mp3_links = browser.find_elements_by_partial_link_text('Part')
-
-        print(book_elem[book], " the book")
-        print("In the book there are {} sections" .format(len(mp3_links)))        
-
-        for chapter in range(len(mp3_links)):
-        #for chapter in range(min(1,len(mp3_links))):
-            print("in the chapter loop, round ", chapter)
-
-            mp3_links = browser.find_elements_by_partial_link_text('Chapter')
-            
-            if len(mp3_links) < 1:
-                mp3_links = browser.find_elements_by_partial_link_text('ntire book')
-            if len(mp3_links) < 1:
-                mp3_links = browser.find_elements_by_partial_link_text('Part')
-
-            print("Chapter ", chapter)
-            mp3_links[chapter].click()
-
-            mp3_url = browser.current_url
-            mp3_response = requests.get(mp3_url)
-            #add book title to file or go fancy and figure out how to add title/author/type as mp3 meta-info
-            #eyeD3
-            file_name = os.path.split(mp3_url)[1]
-            print("File ",file_name)
-
-            mp3_file = os.path.split(mp3_url)[1]
-            directory = "./DanielPinkwaterAudiobooks"
-            file_path = os.path.join(directory,mp3_file)
-
-            with open(file_path, 'wb') as f:
-                for chunk in mp3_response.iter_content(100000):
-                    f.write(chunk)
-
-            browser.back()
-            
-            print(len(mp3_links), "# of chapter")
-            book_elem = browser.find_elements_by_tag_name('Option')
-    '''
+    #Get page for book listing all mp3 audiobook chapters
+    #Doesn't work yet
+    for option in book_options:
+        get_book_page(option)
+        audiobook_dir = make_book_directory(option)
+        links_or_soup = parse_page()
+        get_mp3s(links_or_soup)
 
 if __name__ == '__main__':
     main()
