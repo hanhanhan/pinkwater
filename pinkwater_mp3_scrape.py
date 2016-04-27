b08567ad1d587bd5055df84dca667cd84d52ad56
Hannah Lazarus <han.lazarus@gmail.com>
2016-04-27 11:20:28 -0700

    Fully working program

--- pinkwater_mp3_scrape.py
+++ pinkwater_mp3_scrape.py
@@ -8,12 +8,11 @@ import json
 from bs4 import BeautifulSoup, SoupStrainer
 from fake_useragent import UserAgent
 
-
 def get_book_audiofiles_index():
     pass
 
 def get_book_list():
-    ''' Get list of all audiobook options
+    ''' Get list of all audiobooks from drop-down options
     '''
     booklist = 'booklist.json'
 
@@ -26,7 +25,10 @@ def get_book_list():
     strainer = SoupStrainer('option')
     indexsoup = BeautifulSoup(r.text, 'lxml', parse_only=strainer)
 
-    book_options = [ book['value'] for book in indexsoup.find_all('option')]
+
+    book_options = []
+    for book in indexsoup.find_all('option'):
+        book_options.append(book)
 
     with open(booklist, 'w') as f:
         json.dump(book_options, f)
@@ -34,12 +36,15 @@ def get_book_list():
     return book_options
 
 def get_book_page(book_option):
-    ''' Does not return book specific page with mp3 file index
+    ''' Should: 
+    Make a directory for storing individual web pages for each book if it doesn't exist.
+    Save audiobook-specific page with audiobook chapters/mp3 files
+    
     '''
     #Should I return file path or text (not sure how to get that)
 
-    #check if page is already cached, get if not
-    book_html = get_filesafe_name(book_option)
+    #check if page is already cached, save if not
+    book_html = get_filesafe_name(book_option, '.html')
     book_html_dir = 'bookpages'
     book_html_relativepath = os.path.join(book_html_dir, book_html)
 
@@ -50,89 +55,98 @@ def get_book_page(book_option):
         #book_option = 'Adventures+of+a+Cat-Whiskered+Girl'
         headers = { 'User-Agent' : UserAgent().firefox }
         data = {'thebook' : book_option }
+        
         #data = 'thebook=Adventures+of+a+Cat-Whiskered+Girl'
         response = requests.post('http://www.pinkwater.com/podcast/audioarchive.php', headers=headers, data=data)
         with open(book_html_relativepath,'w') as file:
             file.write(response.text)
 
-    #return response.text / how do I load this?
     return book_html_relativepath
 
-def parse_page(book_html_relativepath):
+def parse_page(book='Wempires', book_folder_path='DanielPinkwaterAudiobooks/Wempires', book_html_relativepath='bookpages/Wempires.html'):
+    ''' Get the urls for each chapter of the audiobook from webpage.
+    '''
 
-    #need to test the 'href'=True part
-    strainer = SoupStrainer('a', 'href=True')
+    strainer = SoupStrainer('a')
 
     with open(book_html_relativepath, 'r') as file:
         book_page_soup = BeautifulSoup(file, 'lxml', parse_only=strainer)
 
+    #for future use labelling mp3s
     audiobook_chapters = {}
-    
+    audiobook_chapters['author'] = 'Daniel Pinkwater'
+    audiobook_chapters['album'] = book
+
     for link in book_page_soup:
-        #either ue this or the strainer with 'href'=True
-        #if 'href' in getattr(link, 'attrs', {}):
-        if '.mp3' in link['href']:
-            get_filesafe_name(link.string)
+        if 'attrs' in link.__dict__.keys():
+            if 'href' in link.attrs and 'mp3' in link.attrs['href']:
+                #Get filesafe name for mp3 combining book+chapter names
+                filesafe_book = get_filesafe_name(book)
+                mp3_name = filesafe_book+'_'+link.text+'.mp3'
+                 
+                #Exit function if file is already saved.
+                mp3_path = os.path.join(book_folder_path, mp3_name)
+                if os.path.exists(mp3_path):
+                    return
+                #Get url for mp3 file
+                url = 'http://www.pinkwater.com/podcast/' + link.attrs['href']
+                response = requests.get(url)
 
-            #Map URL to chapter title 
-            #to test
-            audiobook_chapters[ link['href'] ] = link.text
+                with open(mp3_path,'wb') as file:
+                    file.write(response.content)
 
-            #get file and save to directory
-            #if it doesn't already exist
-
-    return audiobook_chapters
+                audiobook_chapters['book_and_chapter'] = 'url'
 
 def make_book_directory(book):
-    ''' Make a DaneilePinkwaterAudiobook file, and make individual subdirectories
+    ''' Make a DanielPinkwaterAudiobook folder, and make individual book folder
     to hold the multiple chapter audiobook files for each book.
     '''
 
     if not os.path.exists('DanielPinkwaterAudiobooks'):
         os.mkdir('DanielPinkwaterAudiobooks')
+
+    book_folder = get_filesafe_name(book)
+    book_folder_path = os.path.join('DanielPinkwaterAudiobooks', book_folder)
     
-    cwd = os.getcwd()
-    audiobook_subdirectory = os.path.join(cwd, 'DanielPinkwaterAudiobooks', book)
-    
-    if not os.path.exists(audiobook_subdirectory):
-        os.mkdir(audiobook_subdirectory)
+    filepath_check(book_folder_path)
 
-    return audiobook_subdirectory
+    return book_folder_path
 
-def get_filesafe_name(link):
-    reg = re.findall('\w*', link)
-    filesafe_name_url = ''.join(reg) + '.html'
+def get_filesafe_name(name, suffix=''):
+    reg = re.findall('\w*', name)
+    filesafe_name = '_'.join(reg).rstrip('_') + suffix
 
-    return filesafe_name_url
+    return filesafe_name
 
 def filepath_check(path):
     if os.path.exists(path):
-        return True
+        return 
     
     os.mkdir(path)
-    return False
+
 
 def main():
-    ''' Get audiobook files from Daniel Pinkwater's site and save in 
+    ''' Get audiobook files from Daniel Pinkwater's site.
     '''
     #Get list of book options from drop down menu on main page
     book_options = get_book_list()
+    #Make folder to hold all individual book folders
+    filepath_check('DanielPinkwaterAudiobooks')
 
-    #Get page for book listing all mp3 audiobook chapters
-    #Doesn't work yet
-    for option in book_options:
-        
+    for book in book_options:
+
+        #Get cached or requested html file with audiobook mp3 chapters listed
+        mp3s_page = get_book_page(book)
+
         #Make directory for chapter files for each audiobook
-        audiobook_dir = make_book_directory(option)
-        
-        #Get cached or requested html file with audiobook files
-        mp3s_page = get_book_page(option)
-        
-        #Parse out mp3 URLs from html
-        links_or_soup = parse_page(mp3s_page)
-        
-        #Loop through each mp3 url to make sure it's saved in directory
-        get_mp3s(links_or_soup)
+        book_folder = get_filesafe_name(book)
+        book_folder_path = os.path.join('DanielPinkwaterAudiobooks', book_folder)
+        filepath_check(book_folder_path)
+
+        print(book)
+        #Parse out mp3 URLs from html, and save mp3 if not already saved.
+        parse_page(book, book_folder_path, mp3s_page)
+
 
 if __name__ == '__main__':
     main()
